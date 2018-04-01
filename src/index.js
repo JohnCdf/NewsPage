a31ed1ffaa521ae94c6881a3fd5fd7c895672862
John S <30275860+JohnCdf@users.noreply.github.com>
2018-03-31 22:34:13 -0400

    Sh

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import Article from './views/Article';
 import firebase from 'firebase';
 import registerServiceWorker from './registerServiceWorker';
 var config = {
-    apiKey: "AIzaSyANNSUN569tWNvGefPSL2Eb9CCDYFQk7fQ",
-    authDomain: "notic-c48d6.firebaseapp.com",
-    databaseURL: "https://notic-c48d6.firebaseio.com",
-    projectId: "notic-c48d6",
-    storageBucket: "notic-c48d6.appspot.com",
-    messagingSenderId: "42657920325"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
   firebase.initializeApp(config);
 
