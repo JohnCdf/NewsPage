6c644e6b77b3afd57838c4238f0835c47d52dc4e
Jonathan Díaz <30275860+SKEPDIMI@users.noreply.github.com>
2018-08-05 21:59:00 -0400

    Syntax styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter,Route,Switch,Redirect} from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import './assets/stylesheets/bulma.css';
 
@@ -10,18 +10,16 @@ import './App.css';
 
 import firebase from 'firebase';
 
-import firebaseConfig from './config.js'//We dont want this to be public
+import firebaseConfig from './config.js' //We dont want this to be public
 
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render((
     <BrowserRouter>
-    <Switch>
-    <Route exact path="/" render={() => (
-      <Redirect to="/home"/>
-    )}/>
-    <Route exact path="/home" component={Home}/>
-    <Route exact path="/article" component={Article}/>
-    </Switch>
+      <Switch>
+          <Route exact path="/" render={() => <Redirect to="/home"/>}/>
+          <Route exact path="/home" component={Home}/>
+        <Route exact path="/article" component={Article}/>
+      </Switch>
     </BrowserRouter>
 ), document.getElementById('root'));
